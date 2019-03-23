3f8c34c0a9832a760566289beff087dbdfd26829
Norman Choo <leftoverrice@gmail.com>
2019-03-24 00:26:31 +0800

    Putting all existing ex files into repo

---
+++ ex2.py
@@ -0,0 +1,8 @@
+# A comment line 1
+# comment line 2
+print("I could have code like this") # and the comment after is ignored
+
+# another comment linke
+# another comment line again
+
+print("This will @@@ ### run")
